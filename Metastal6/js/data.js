9883df4aa4489f1b8ef7dcd7717f32cb271aa6d2
Kapikologamer <62207823+Kapikologamer@users.noreply.github.com>
2021-12-30 00:28:46 +0100

    Add files via upload

---
+++ Metastal6/js/data.js
@@ -0,0 +1,60 @@
+export default {
+    products: [
+        {
+            _id:'1',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'2',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'3',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'4',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'5',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'6',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'7',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+        {
+            _id:'8',
+            name:"Nogi do stolika",
+            image:"../images/productsImage/nogi/NogiDoStolika.png",
+            price:"160",
+            cointInStock:"10",
+        },
+    ],
+};
