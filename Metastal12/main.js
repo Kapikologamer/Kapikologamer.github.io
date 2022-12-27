5ac8f34615ce26f022a36a65438dc88374a6f1dc
Kapikologamer <62207823+Kapikologamer@users.noreply.github.com>
2022-12-27 21:15:53 +0100

    Add files via upload

--- Metastal12/main.js
+++ Metastal12/main.js
@@ -1,3 +1,7 @@
 window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
+  window.scrollTo(0, 0);
+}
+
+function gotoshop() {
+  window.open('https://www.yoursite.com/mypage', '_blank')
+}
